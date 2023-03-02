8f428f18fb6594a81c725db4891ddd78f1615e08
chevalme arnaud <arnaud.chevalme@edu.univ-fcomte.fr>
2023-03-02 09:34:45 +0100

    première piste artiste

---
+++ app/src/main/java/com/example/fimudroid/data/DataSource.kt
@@ -0,0 +1,15 @@
+package com.example.fimudroid.data
+//
+//import com.example.fimudroid.models.Artiste
+//
+//class DataSource {
+//    val artistes = List<Artiste>
+//
+//    val artiste0 = Artiste(
+//        0,
+//        "Blabla",
+//
+//    );
+//
+//    return
+//}
