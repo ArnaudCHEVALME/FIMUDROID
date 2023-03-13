58bfc3755ceccd38cb205ea2ba85298836285917
hfoulon <foulhugo25620@gmail.com>
2023-03-13 10:27:59 +0100

    Update Artiste

--- app/src/main/java/com/example/fimudroid/ui/artistes/ArtisteDetailsFragment.kt
+++ app/src/main/java/com/example/fimudroid/ui/artistes/ArtisteDetailsFragment.kt
@@ -1,6 +1,7 @@
 package com.example.fimudroid.ui.artistes
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -13,6 +14,8 @@ class ArtisteDetailsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val artiste_id:Int = arguments?.getInt("id_art") ?: -1
+        Log.i("LES ARTISTE PUTAIN CHAR TU VAS LES BOUFFER LA MERDE", artiste_id.toString())
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.artiste_details_fragment, container, false)
     }
