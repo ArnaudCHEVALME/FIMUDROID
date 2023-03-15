8f6df9a29aec958b4428a7600472ad867bc9e41a
DupuySamson <samson.dupuy@edu.univ-fcomte.fr>
2023-03-15 12:49:36 +0100

    Updrage fondu au noir, mtn fondu au blanc

--- app/src/main/java/com/example/fimudroid/ui/news/NewsDetails.kt
+++ app/src/main/java/com/example/fimudroid/ui/news/NewsDetails.kt
@@ -1,5 +1,6 @@
 package com.example.fimudroid.ui.news
 
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -30,17 +31,20 @@ class NewsDetails(
             this, ViewModelProvider.AndroidViewModelFactory(requireActivity().application)
         )[NewsViewModel::class.java]
 
-        val news_id:Int = arguments?.getInt("news_id") ?: -1
+        val news_id:Int = arguments?.getInt("news_id") ?: 0
         Log.i("LES DATA PUTAIN MERDE CHAR TU VAS LES BOUFFER TES NEWS", news_id.toString())
-//        Toast.makeText(context, "id news: ${news.value.titre}", Toast.LENGTH_LONG).show()
         val root = inflater.inflate(R.layout.news_details_fragment, container, false)
+        val title:TextView = root.findViewById(R.id.actu_title)
+        val core:TextView = root.findViewById(R.id.actu_core)
+        val bar:View = root.findViewById(R.id.side_bare_news)
+        bar.setBackgroundColor(Color.parseColor("#FFF5B1"))
+        title.text = "Chargement..."
+        core.text = "Chargement..."
         lifecycleScope.launch(Dispatchers.IO){
             val news = viewModel.getById(news_id)
-            Log.i("VALUES", news.toString())
-            val title:TextView = root.findViewById(R.id.actu_title)
-            val core:TextView = root.findViewById(R.id.actu_core)
             title.text = news[0].titre
             core.text = news[0].contenu
+
         }
         return root
     }
