88b7884c5585e27c8de385e1d6bfffc7737bbc72
DupuySamson <samson.dupuy@edu.univ-fcomte.fr>
2023-03-15 14:09:13 +0100

    Waaa c'est un peut plus beau les news la

--- app/src/main/java/com/example/fimudroid/ui/news/NewsDetails.kt
+++ app/src/main/java/com/example/fimudroid/ui/news/NewsDetails.kt
@@ -36,6 +36,7 @@ class NewsDetails(
         val root = inflater.inflate(R.layout.news_details_fragment, container, false)
         val title:TextView = root.findViewById(R.id.actu_title)
         val core:TextView = root.findViewById(R.id.actu_core)
+        val date:TextView = root.findViewById(R.id.detail_news_date)
         val bar:View = root.findViewById(R.id.side_bare_news)
         bar.setBackgroundColor(Color.parseColor("#FFF5B1"))
         title.text = "Chargement..."
@@ -44,6 +45,7 @@ class NewsDetails(
             val news = viewModel.getById(news_id)
             title.text = news[0].titre
             core.text = news[0].contenu
+            date.text = news[0].date_envoi?.subSequence(0, 10) ?: "no Date"
 
         }
         return root
