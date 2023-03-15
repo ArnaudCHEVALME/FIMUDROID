4c7e9ce3272b91898eb67232ee3752dc78130ab7
hfoulon <foulhugo25620@gmail.com>
2023-03-15 23:11:23 +0100

    Image IF ELSE IF ELSE IF yandere dev <3

--- app/src/main/java/com/example/fimudroid/ui/artistes/ArtistAdapter.kt
+++ app/src/main/java/com/example/fimudroid/ui/artistes/ArtistAdapter.kt
@@ -4,6 +4,7 @@ import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.fimudroid.R
@@ -19,6 +20,7 @@ class ArtistAdapter(
 
     class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
         val textView: TextView = view.findViewById(R.id.artist_name_text_view)
+        val imageView: ImageView = view.findViewById(R.id.imageButton)
     }
 
 
@@ -36,6 +38,25 @@ class ArtistAdapter(
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         val artiste = dataset[position]
         holder.textView.text = artiste.nom
+
+        val drawableResource = when (artiste.id) {
+            1 -> R.drawable.ma_joye
+            2 -> R.drawable.ma_pales
+            3 -> R.drawable.ma_grayssoker
+            4 -> R.drawable.ma_nastyjoe
+            5 -> R.drawable.ma_poligone
+            6 -> R.drawable.ma_romainmuller
+            7 -> R.drawable.ma_tomrochet
+            8 -> R.drawable.ma_oceya
+            9 -> R.drawable.ma_encore
+            10 -> R.drawable.ma_encore
+            else -> R.drawable.ma_cloud // Placeholder image when there is no matching drawable resource
+        }
+
+        holder.imageView.setImageResource(drawableResource)
+
+
+
         holder.itemView.setOnClickListener{println(artiste.id)}
 
         holder.itemView.setOnClickListener{
