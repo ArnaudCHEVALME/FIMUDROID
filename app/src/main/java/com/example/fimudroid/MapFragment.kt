40e9c264d302542c09e29d61794945f7df67a46a
Gabs <gab.blond@hotmail.com>
2023-03-16 01:38:47 +0100

    Add working floating button and ass details to scene and stand

--- app/src/main/java/com/example/fimudroid/MapFragment.kt
+++ app/src/main/java/com/example/fimudroid/MapFragment.kt
@@ -1,19 +1,21 @@
 package com.example.fimudroid
 
+import android.content.Context.LAYOUT_INFLATER_SERVICE
 import android.os.Bundle
 import android.preference.PreferenceManager
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.ViewModelProvider
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
-import com.example.fimudroid.network.models.Stand
 import com.example.fimudroid.network.FimuApiService
-import com.example.fimudroid.network.models.Artiste
+import com.example.fimudroid.network.models.Scene
+import com.example.fimudroid.network.models.Service
+import com.example.fimudroid.network.models.Stand
 import com.example.fimudroid.network.retrofit
-import com.example.fimudroid.ui.stands.StandViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
@@ -21,17 +23,11 @@ import org.osmdroid.config.Configuration
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.MapView
-import org.osmdroid.views.overlay.ItemizedIconOverlay
-import org.osmdroid.views.overlay.ItemizedOverlayWithFocus
 import org.osmdroid.views.overlay.Marker
-import org.osmdroid.views.overlay.OverlayItem
+import org.osmdroid.views.overlay.infowindow.InfoWindow
+import org.osmdroid.views.overlay.infowindow.MarkerInfoWindow
+import org.w3c.dom.Text
 
-
-/**
- * A simple [Fragment] subclass.
- * Use the [MapFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class MapFragment : Fragment() {
 
     private val REQUEST_PERMISSIONS_REQUEST_CODE = 1
@@ -47,22 +43,24 @@ class MapFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         super.onCreateView(inflater, container, savedInstanceState)
+        var root = inflater.inflate(R.layout.fragment_map,container,false)
+        map = root.findViewById(R.id.mapView)
 
-        map = MapView(inflater.context)
+        Configuration.getInstance().load(requireContext(), PreferenceManager.getDefaultSharedPreferences(requireContext()))
+        map.setTileSource(TileSourceFactory.MAPNIK)
+
+        val mapController = map.controller
+        mapController.setZoom(18.5)
+
+        val startPoint = GeoPoint( 47.638410197922674,6.862777328835964)
+        mapController.setCenter(startPoint)
 
         lifecycleScope.launch {
             val stands: List<Stand> = withContext(Dispatchers.IO) {
                 api.getStands().data
             }
 
-            Configuration.getInstance().load(requireContext(), PreferenceManager.getDefaultSharedPreferences(requireContext()))
-            map.setTileSource(TileSourceFactory.MAPNIK)
 
-            val mapController = map.controller
-            mapController.setZoom(18.5)
-
-            val startPoint = GeoPoint( 47.638410197922674,6.862777328835964)
-            mapController.setCenter(startPoint)
 
             //your items
             /*val items = ArrayList<OverlayItem>()
@@ -87,26 +85,65 @@ class MapFragment : Fragment() {
             map.overlays.add(overlay)*/
             for (stand: Stand in stands){
                 var markerStand = Marker(map)
-                markerStand.setPosition(GeoPoint(stand.longitude.toDouble(),stand.latitude.toDouble()))
-                markerStand.setTitle(stand.libelle)
+                markerStand.setPosition(GeoPoint(stand.latitude.toDouble()+0.0005,stand.longitude.toDouble()))
+                var titre = stand.libelle+"\n========="
+                for(service: Service in stand.services){
+                    titre +="\n- "+service.libelle
+                }
+                markerStand.setTitle(titre)
+                //Log.i("MAP",stand.libelle)
                 markerStand.setIcon(resources.getDrawable(R.drawable.stand))
+
+                //markerStand.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+
+                markerStand.setPanToView(true)
+
+
                 map.overlays.add(markerStand)
+
+
+                //markerStand.setOnMarkerClickListener()
             }
 
-            var sceneTestMarker = Marker(map)
+            /*var sceneTestMarker = Marker(map)
             sceneTestMarker.setPosition(GeoPoint(47.63830808584398,6.8630603018852705))
             sceneTestMarker.setTitle("Le Kiosque")
             sceneTestMarker.setIcon(resources.getDrawable(R.drawable.microphone))
             map.overlays.add(sceneTestMarker)
-
+*/
             var standTestMarker = Marker(map)
             standTestMarker.setPosition(GeoPoint(47.638410197922674,6.862777328835964))
             standTestMarker.setTitle("La belle bête")
             standTestMarker.setIcon(resources.getDrawable(R.drawable.stand))
+            standTestMarker.isDraggable()
+
+
+            //standTestMarker.setInfoWindow(infoWindow)
+
             map.overlays.add(standTestMarker)
+
+
         }
 
-        return map
+        lifecycleScope.launch {
+            val scenes: List<Scene> = withContext(Dispatchers.IO){
+                api.getScenes().data
+            }
+
+            for (scene : Scene in scenes){
+                var sceneMarker : Marker = Marker(map)
+                sceneMarker.setPosition(GeoPoint(scene.latitude.toDouble(),scene.longitude.toDouble()))
+                var titre = scene.libelle+"\n=========\n"+scene.typescene.libelle
+                sceneMarker.setTitle(titre)
+                sceneMarker.setIcon(resources.getDrawable(R.drawable.microphone))
+                sceneMarker.setPanToView(true)
+                map.overlays.add(sceneMarker)
+            }
+
+
+        }
+
+        return root
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
